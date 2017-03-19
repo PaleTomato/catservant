4f47edbca6c76598fe4ad0cf8c01c8143733858e
Patrick Leedham <ptl76@hotmail.co.uk>
2017-03-19 10:14:06 +0000

    Setup initial scaffolding

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='catservant',
+      version='0.1',
+      description='Python package for automated pet feeding',
+      url='https://github.com/PaleTomato/catservant',
+      author='Patrick Leedham',
+      author_email='ptl76@hotmail.co.uk',
+      license='GPL-3.0',
+      packages=['catservant'])
